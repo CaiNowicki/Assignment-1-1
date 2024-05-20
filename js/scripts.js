2eb423d165e0e38e11b7973a5bea8687c16fdb2e
Cai Nowicki <cai.nowicki@gmail.com>
2024-05-19 23:43:07 -0400

    fixed image alt text...sort of. they display now, but it's behind the image instead of in front.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 // Get the login button and hover text elements by their IDs
 var loginButton = document.getElementById('loginButton');
 var hoverText = document.getElementById('hoverText');
+// Select all images from the page
+var images = document.querySelectorAll('.resized-image')
 
 // Add event listener for mouseover event on the login button
 loginButton.addEventListener('mouseover', function() {
@@ -12,4 +14,22 @@ loginButton.addEventListener('mouseover', function() {
 loginButton.addEventListener('mouseout', function() {
     // Hide the hover text when the mouse leaves the button
     hoverText.style.display = 'none';
+});
+
+
+
+images.forEach(function(image) {
+    // add check for mouseover to each image
+    image.addEventListener('mouseover', function(event) {
+        // if yes, get alt text
+        var altText = event.target.alt || 'No Image ID available'; //fetch alt text or use default
+        hoverText.textContent = altText;
+        hoverText.style.display = 'block';
+    }
+);
+// check for mouseout on each image
+image.addEventListener('mouseout', function() {
+    // mouse moves off, hide alt text
+    hoverText.style.display = 'none';
+});
 });
